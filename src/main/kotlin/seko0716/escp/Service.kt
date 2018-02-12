58a9b14d839f11227bc85addd06d6b0451934e30
seko0716 <sergey3440@gmail.com>
2018-02-12 10:21:54 +0400

    add method from/to file

--- src/main/kotlin/seko0716/escp/Service.kt
+++ src/main/kotlin/seko0716/escp/Service.kt
@@ -1,10 +1,13 @@
 package seko0716.escp
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.shell.standard.ShellComponent
 import org.springframework.shell.standard.ShellMethod
 import org.springframework.web.client.RestTemplate
+import java.io.File
 
 
 @ShellComponent
@@ -41,6 +44,40 @@ class Service {
         val to: String? = nameToUrl[serverTo]
         if (from == null || to == null) return "not found server by nae"
         return perform(serverFromUrl = from, request = request, serverToUrl = to)
+    }
+
+    @ShellMethod(""""copy from one es server to file. es server port is 9201.
+        request is https://www.elastic.co/guide/en/elasticsearch/reference/current/search-search.html
+        sample: cp_file dev1 index_name/_search""")
+    fun cp_file(serverFrom: String, request: String): String {
+        val from: String = nameToUrl[serverFrom] ?: return "not found server by nae"
+
+        val result = restTemplate.getForObject("$from:9201/$request", Result::class.java)
+                ?: return "failed request"
+        val mapper = jacksonObjectMapper()
+        val writeValueAsString = mapper.writeValueAsString(result)
+
+        val file = File("${request.split("/")[0]}.json")
+        file.createNewFile()
+
+        file.bufferedWriter().use { out -> out.write(writeValueAsString) }
+
+        return "done for ${result.getData().size}"
+    }
+
+    @ShellMethod(""""copy from file to es server. es server port is 9201.
+        request is https://www.elastic.co/guide/en/elasticsearch/reference/current/search-search.html
+        sample: cp_from_file fileName dev2""")
+    fun cp_from_file(fileName: String,serverToUrl: String): String {
+        val to: String = nameToUrl[serverToUrl] ?: return "not found server by nae"
+        val mapper = jacksonObjectMapper()
+        val result = mapper.readValue<Result>(File(fileName))
+
+        result.getData().forEach {
+            restTemplate.put("$to:9201/${it._index}/${it._type}/${it._id}", it._source)
+            println("$serverToUrl:$9201/${it._index}/${it._type}/${it._id}")
+        }
+        return "done for ${result.getData().size}"
     }
 
 
